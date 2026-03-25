27b1bd56b30cf56347b2befb300035c07c04ec3f
ChocolateDingo <spencerrmckay@gmail.com>
2026-03-25 17:56:38 -0400

    font and colors

--- sketch.js
+++ sketch.js
@@ -1,16 +1,42 @@
 function preload() {
-img = loadImage('heart.png');
-  
+
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+  myFont = loadFont('Jersey10.ttf'); 
 }
 
 
 function draw() {
   background(220);
   background('#000000');
+  textSize(150);
+  textFont (myFont);
+  textAlign (CENTER);
+  text('CASH GRAB', windowWidth/2, windowHeight/2);
+  fill(255);
+
+  textSize(60);
+  textFont (myFont);
+  textAlign (CENTER);
+  fill('#FFEB3B');
+  text('1P', windowWidth/20, windowHeight/11);
+  
+   textSize(80);
+  textFont (myFont);
+  textAlign (CENTER);
+  fill('#FFEB3B');
+  text('START', windowWidth/2, windowHeight/1.65);
+  
+  textSize(60);
+  textFont (myFont);
+  textAlign (CENTER);
+  fill('#FFEB3B');
+  fill('#4CAF50');
+  text('$000', windowWidth/1.07,windowHeight/11); 
+  
+ 
+  
   
 }
