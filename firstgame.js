70caf0d05507cf78d958c7cd1b045918ac0a8773
ChocolateDingo <spencerrmckay@gmail.com>
2026-04-13 09:28:35 -0400

    chore: clean up empty code change sections in the changes log

--- firstgame.js
+++ firstgame.js
@@ -1,10 +1,37 @@
+let clickCount = 0;
+
+
 function preload() {}
+xpos = 1;
+xspeed = 0;
+
+xpos2 = 1;
+xspeed2 = 0;
+
+xpos3 = 1;
+xspeed3 = 0;
+
+xpos4 = 1;
+xspeed4 = 0;
+
+let img2;
+let img3;
+let img4;
+let img5;
+let img4WasClicked = false;
+let img2WasClicked = false;
+let img3WasClicked = false;
+let img5WasClicked = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   myFont = loadFont("Jersey10.ttf");
 
   img = loadImage("game1.png");
+  img2 = loadImage("Drump.png");
+  img3 = loadImage("Jezos2.png");
+  img4 = loadImage("Bates.png");
+  img5 = loadImage("Muckerberg.png");
 }
 
 function draw() {
@@ -27,8 +54,103 @@ function draw() {
   textFont(myFont);
   textAlign(CENTER);
   fill("#000000");
-  text("CLICK THE BILLIONARES TO GET RID OF THEM", windowWidth /2, windowHeight /10);
+  text(
+    "CLICK THE BILLIONARES TO GET RID OF THEM",
+    windowWidth / 2,
+    windowHeight / 10,
+  );
 
+  if (img4WasClicked == false) {
+    image(img4, xpos, 600, 300, 300);
+    if (xpos > 2) {
+      xspeed = 2;
+    }
+    if (xpos < 2) {
+      xspeed = 2;
+    }
+  }
+  if (img5WasClicked == false) {
+    image(img5, xpos2, 620, 300, 300);
+    if (xpos2 > 2) {
+      xspeed2 = 4;
+    }
+    if (xpos2 < 2) {
+      xspeed2 = 4;
+    }
+  }
 
+  if (img2WasClicked == false) {
+    image(img2, xpos3, 500, 300, 300);
+    if (xpos3 > 2) {
+      xspeed3 = 0.5;
+    }
+    if (xpos3 < 2) {
+      xspeed3 = 0.5;
+    }
+  }
 
+  if (img3WasClicked == false) {
+    image(img3, xpos4, 550, 300, 300);
+    if (xpos4 > 2) {
+      xspeed4 = 6;
+    }
+    if (xpos4 < 2) {
+      xspeed4 = 6;
+    }
+  }
+
+  xpos += xspeed;
+  xpos2 += xspeed2;
+  xpos3 += xspeed3;
+  xpos4 += xspeed4;
 }
+
+// function mousePressed() {
+//   clickCount++;
+
+//   if (clickCount >= 4) {
+//     window.location.href = "firstwin.html";
+//   }
+// }
+
+function mousePressed() {
+  if (
+    mouseX > 1 && //if the mouse is greather than 200 we're over the image
+    mouseX < 1000 && //if the mouse is less than 300 were over the image (since the image is at 200 and is 100 wide = 300)
+    mouseY > 1 && //same idea but on the vertical axis.
+    mouseY < 500
+  ) {
+    img2WasClicked = true;
+  }
+  if (
+    mouseX > 1000 && //if the mouse is greather than 200 we're over the image
+    mouseX < 2000 && //if the mouse is less than 300 were over the image (since the image is at 200 and is 100 wide = 300)
+    mouseY > 1 && //same idea but on the vertical axis.
+    mouseY < 500
+  ) {
+    img3WasClicked = true;
+  }
+  if (
+    mouseX > 1000 && //if the mouse is greather than 200 we're over the image
+    mouseX < 2000 && //if the mouse is less than 300 were over the image (since the image is at 200 and is 100 wide = 300)
+    mouseY > 500 && //same idea but on the vertical axis.
+    mouseY < 1000
+  ) {
+    img4WasClicked = true;
+  }
+  if (
+    mouseX > 1 && //if the mouse is greather than 200 we're over the image
+    mouseX < 1000 && //if the mouse is less than 300 were over the image (since the image is at 200 and is 100 wide = 300)
+    mouseY > 500 && //same idea but on the vertical axis.
+    mouseY < 1000
+  ) {
+    img5WasClicked = true;
+  }
+
+  clickCount++;
+
+  if (clickCount >= 4) {
+    window.location.href = "firstwin.html";
+  }
+}
+
